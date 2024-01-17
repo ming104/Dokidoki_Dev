13300ea219d64d307f1166adb588417b3459da53
ming104 <jangminguo@gmail.com>
2024-01-17 23:03:35 +0900

    animation 개선

---
+++ Assets/Script/Exam/Anim_Change.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class Anim_Change : MonoBehaviour
+// {
+//     public Image Image1;
+//     public Image Image2;
+
+//     public RectTransform rectTransform;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+
+//     }
+
+//     public IEnumerator Changeimage()
+//     {
+//         rectTransform.
+//         yield return;
+//     }
+// }
